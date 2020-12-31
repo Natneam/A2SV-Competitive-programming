d73681e273ee96524ca09a4c047a7b5548cf2680
natneam <se.natneam.mesele@gmail.com>
2020-12-31 06:46:12 +0300

    addtion simulation exercise finished

---
+++ Day 1/addition_simulation.py
@@ -0,0 +1,27 @@
+# Calculate a + b
+
+def add(num1, num2):
+    # casting the numbers to comfortable format
+    strNum1, strNum2 = str(num1), str(num2)
+    if len(strNum1) > len(strNum2):
+        for i in range(len(strNum1)-len(strNum2)):
+            strNum2 = '0' + strNum2
+    elif len(strNum2) > len(strNum1):
+        for i in range(len(strNum2)-len(strNum1)):
+            strNum1 = '0' + strNum1
+
+    carry = 0
+    summation = ''
+    for i in range(len(strNum1)):
+        s = str(int(strNum1[-i-1]) + int(strNum2[-i-1]) + carry)
+        # print(int(strNum2[i-len(strNum2)]))
+        if len(s) == 2:
+            carry = int(s[0])
+        else:
+            carry = 0
+        summation = s[-len(s)] + summation
+    print(summation);
+
+
+
+add(123753959805305, 2345643853094036963)
