b973671f043fce60435b195653289e11877bf7b1
natneam <se.natneam.mesele@gmail.com>
2021-01-11 17:31:06 +0300

    Middle of the Linked List problem solved

---
+++ Day 7/middle_of_the_linked_list.py
@@ -0,0 +1,25 @@
+# LINK TO THE PROBLEM => https://leetcode.com/problems/middle-of-the-linked-list/
+
+Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+# the answer is the code below
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        # finding the length of the linked list
+        length = 0
+        current = head
+        while (current!= None):
+            length += 1
+            current = current.next
+        
+        current = head
+        index = 1
+        while (index < (length// 2) + 1):
+            index+=1
+            current = current.next
+        return current
+        
+        
