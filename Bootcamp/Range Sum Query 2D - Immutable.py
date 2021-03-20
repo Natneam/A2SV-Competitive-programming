13fd03a98aa9e953f948e812ebdd1bb2896cfda2
Natneam Mesele <58377289+Natneam@users.noreply.github.com>
2021-03-19 18:07:33 -0700

    Update Range Sum Query 2D - Immutable

--- Bootcamp/Range Sum Query 2D - Immutable.py
+++ Bootcamp/Range Sum Query 2D - Immutable.py
@@ -4,13 +4,25 @@ class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
         self.matrix = matrix
+        self.prefixMatrix = []
+        for row in self.matrix:
+            prefixSum = [row[0]]
+            for j in range(1,len(row)):
+                prefixSum.append(row[j]+prefixSum[j-1])
+            self.prefixMatrix.append(prefixSum)
+        print(self.prefixMatrix)
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         summation = 0
-        for x in range(row1, row2+1):
-            for y in range(col1, col2+1):
-                summation += self.matrix[x][y]
+        for row in range(row1, row2+1):
+            if col1 == 0:
+                summation += self.prefixMatrix[row][col2]
+            elif col2 == 0:
+                return 0
+            else:
+                summation += self.prefixMatrix[row][col2] - self.prefixMatrix[row][col1-1]
         return summation
+        
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
